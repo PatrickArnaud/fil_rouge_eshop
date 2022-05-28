3381eff4b465a3885f43f1d9a7ce84862f71101e
PatrickArnaud <patrick.arnaud34470@gmail.com>
2022-05-28 12:24:41 +0200

    reset

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo view('common/header.php',);
-        echo view('content/home.php');
-        echo view('common/footer.php');
+        echo view('./Views/common/V_Header.php',);
+        echo view('./Views/common/V_Nav.php',);
+        echo view('./Views/content/V_Home.php');
+        echo view('./Views/common/V_Footer.php');
     }
 }
