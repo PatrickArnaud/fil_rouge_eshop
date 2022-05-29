7aa981b36d317acd8b0e986941a288965c94bebd
PatrickArnaud <patrick.arnaud34470@gmail.com>
2022-05-29 15:27:06 +0200

    adding view

--- app/Views/content/V_Article.php
+++ app/Views/content/V_Article.php
@@ -1,5 +1,110 @@
-<div class="container">
-    <div class="col-12">
-        <h1>Nos Articles :</h1>
+
+<!-- Filtre -->
+
+<div class="filter_search" class="d-flex row-cols-md-3 g-3">
+  <form action="" method="POST" class="col d-flex">
+    <label class="col" >Choisir ingredient :</label>
+    <select class="col" name="ingredientsType">
+        <!-- <option value="Choisir une recette" placeholder="Choisir une recette" aria-disabled="choisir"></option> -->
+      <option value=""></option>
+      <option value="fruit">Pomme</option>
+      <option value="vegetable" >Asperges</option>
+      <option value="red-fruit" >Tomates</option>
+      <option value="green-vegetable" >Haricots verts</option>
+    </select>
+
+    <div class="d-flex">
+        <input class="col" type="text" name="ingredient_name" />
+
+        <input class="col" type="submit" name="search" value="Search" />
     </div>
+      
+
+  </form>
+</div>
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="https://via.placeholder.com/550x750" class="card-img-top default-img"  alt="#">
+      <div class="card-body">
+        <h5 class="card-title">Nom Ingredient</h5>
+        <p class="card-text">Prix €€</p> <br>
+        <p class="card-text">Ajouter au panier</p>
+      </div>
+    </div>
+  </div>
 </div>
